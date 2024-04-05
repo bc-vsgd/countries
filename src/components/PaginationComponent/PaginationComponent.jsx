ab21a09f445cbf909728329c418ba19e66d322ae
bc-vsgd <benoit.charlesbc@yahoo.fr>
2024-04-05 21:26:46 +0200

    Style: pagination comp (+ sorted page, search comp changes)

--- src/components/PaginationComponent/PaginationComponent.jsx
+++ src/components/PaginationComponent/PaginationComponent.jsx
@@ -15,115 +15,125 @@ const PaginationComponent = ({
 }) => {
   return (
     <div className="pagination-comp flex-row">
-      {page > 1 && (
-        <button
-          onClick={() => {
-            if (page > 1) {
-              setPage(page - 1);
+      {page > 1 ? (
+        <div>
+          <button
+            onClick={() => {
+              if (page > 1) {
+                setPage(page - 1);
 
-              if (name) {
-                setSearchParams({
-                  name: name,
-                  page: page - 1,
-                });
+                if (name) {
+                  setSearchParams({
+                    name: name,
+                    page: page - 1,
+                  });
+                }
+                if (pop) {
+                  setSearchParams({
+                    pop: pop,
+                    page: page - 1,
+                  });
+                }
+                if (area) {
+                  setSearchParams({
+                    area: area,
+                    page: page - 1,
+                  });
+                }
+                if (nameSearch) {
+                  setSearchParams({
+                    namesearch: nameSearch,
+                    page: page - 1,
+                  });
+                }
+                if (continent) {
+                  setSearchParams({
+                    cont: continent,
+                    page: page - 1,
+                  });
+                }
+                if (language) {
+                  setSearchParams({
+                    lang: language,
+                    page: page - 1,
+                  });
+                }
+                if (currency) {
+                  setSearchParams({
+                    curr: currency,
+                    page: page - 1,
+                  });
+                }
               }
-              if (pop) {
-                setSearchParams({
-                  pop: pop,
-                  page: page - 1,
-                });
-              }
-              if (area) {
-                setSearchParams({
-                  area: area,
-                  page: page - 1,
-                });
-              }
-              if (nameSearch) {
-                setSearchParams({
-                  namesearch: nameSearch,
-                  page: page - 1,
-                });
-              }
-              if (continent) {
-                setSearchParams({
-                  cont: continent,
-                  page: page - 1,
-                });
-              }
-              if (language) {
-                setSearchParams({
-                  lang: language,
-                  page: page - 1,
-                });
-              }
-              if (currency) {
-                setSearchParams({
-                  curr: currency,
-                  page: page - 1,
-                });
-              }
-            }
-          }}
-        >
-          <FontAwesomeIcon icon="fa-solid fa-angle-left" className="icon" />
-        </button>
+            }}
+          >
+            <FontAwesomeIcon icon="fa-solid fa-angle-left" className="icon" />
+          </button>
+        </div>
+      ) : (
+        // Empty div (page === 1)
+        <div></div>
       )}
       <p>{page}</p>
 
-      {page < maxPage && (
-        <button
-          onClick={() => {
-            if (page < maxPage) {
-              setPage(page + 1);
+      {page < maxPage ? (
+        <div>
+          <button
+            onClick={() => {
+              if (page < maxPage) {
+                setPage(page + 1);
 
-              if (name) {
-                setSearchParams({
-                  name: name,
-                  page: page + 1,
-                });
+                if (name) {
+                  setSearchParams({
+                    name: name,
+                    page: page + 1,
+                  });
+                }
+                if (pop) {
+                  setSearchParams({
+                    pop: pop,
+                    page: page + 1,
+                  });
+                }
+                if (area) {
+                  setSearchParams({
+                    area: area,
+                    page: page + 1,
+                  });
+                }
+                if (nameSearch) {
+                  setSearchParams({
+                    namesearch: nameSearch,
+                    page: page + 1,
+                  });
+                }
+                if (continent) {
+                  setSearchParams({
+                    cont: continent,
+                    page: page + 1,
+                  });
+                }
+                if (language) {
+                  setSearchParams({
+                    lang: language,
+                    page: page + 1,
+                  });
+                }
+                if (currency) {
+                  setSearchParams({
+                    curr: currency,
+                    page: page + 1,
+                  });
+                }
               }
-              if (pop) {
-                setSearchParams({
-                  pop: pop,
-                  page: page + 1,
-                });
-              }
-              if (area) {
-                setSearchParams({
-                  area: area,
-                  page: page + 1,
-                });
-              }
-              if (nameSearch) {
-                setSearchParams({
-                  namesearch: nameSearch,
-                  page: page + 1,
-                });
-              }
-              if (continent) {
-                setSearchParams({
-                  cont: continent,
-                  page: page + 1,
-                });
-              }
-              if (language) {
-                setSearchParams({
-                  lang: language,
-                  page: page + 1,
-                });
-              }
-              if (currency) {
-                setSearchParams({
-                  curr: currency,
-                  page: page + 1,
-                });
-              }
-            }
-          }}
-        >
-          <FontAwesomeIcon icon="fa-solid fa-angle-right" className="icon" />
-        </button>
+            }}
+          >
+            <FontAwesomeIcon icon="fa-solid fa-angle-right" className="icon" />
+          </button>
+        </div>
+      ) : (
+        // Page === max page => empty div
+        <div></div>
       )}
     </div>
   );
